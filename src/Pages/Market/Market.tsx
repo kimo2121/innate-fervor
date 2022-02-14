cfbfeaaae7e54f7672a50b49875055829c607c42
kareem muhammad <kareem.gaad@gmail.com>
2022-02-14 06:01:50 +0200

    .

--- src/Pages/Market/Market.tsx
+++ src/Pages/Market/Market.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import Popup from "../../components/Popup/Popup";
 import panel from "../../assets/panel.png";
@@ -6,7 +6,9 @@ import { ReactComponent as Magnifier } from "../../assets/magnifier.svg";
 import MarketCard from "../../components/MarketCard/MarketCard";
 import SideFilter from "../../components/SideFilter/SideFilter";
 import BasicModal from "../../components/Modal/Modal";
-
+import ReactPaginate from "react-paginate";
+import productsData from "./data";
+import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai";
 interface traitsTypes {
   Data: {
     price: string;
@@ -17,40 +19,49 @@ interface traitsTypes {
 }
 
 const Market = () => {
-  const NextArrow = (onClick: any) => {
-    return (
-      <div className="arrow next" onClick={onClick}>
-        {/* <img src={RightArrow} alt="" className="right-direction" /> */}
-      </div>
-    );
-  };
+  const [users, setUsers] = useState(productsData);
+  const [pageNumber, setPageNumber] = useState(0);
+  const usersPerPage = 8;
+  const pagesVisited = pageNumber * usersPerPage;
 
-  const PrevArrow = (onClick: any) => {
-    return (
-      <div className="arrow prev" onClick={onClick}>
-        {/* <img src={LeftArrow} alt="" className="left-direction" /> */}
-      </div>
-    );
-  };
+  const displayUsers = users
+    .slice(pagesVisited, pagesVisited + usersPerPage)
+    .map((user) => {
+      return <BasicModal navbar={false} />;
+    });
 
+  const pageCount = Math.ceil(users.length / usersPerPage);
+
+  const changePage = ({ selected }: any) => {
+    setPageNumber(selected);
+  };
   return (
     <div className="market">
       <SideFilter />
       <div className="right-slider">
-        <div className="frame-container">
-          <BasicModal navbar={false} />
-          <BasicModal navbar={false} />
-          <BasicModal navbar={false} />
-          <BasicModal navbar={false} />
-          <BasicModal navbar={false} />
-          <BasicModal navbar={false} />
-          <BasicModal navbar={false} />
-          <BasicModal navbar={false} />
-        </div>
+        <div className="frame-container">{displayUsers}</div>
+        <ReactPaginate
+          previousLabel={
+            <AiOutlineArrowLeft
+              color="#ffb657"
+              className="pagination pagination-left"
+            />
+          }
+          nextLabel={
+            <AiOutlineArrowRight
+              color="#ffb657"
+              className="pagination pagination-right"
+            />
+          }
+          pageCount={pageCount}
+          onPageChange={changePage}
+          containerClassName={"paginationBttns"}
+          previousLinkClassName={"previousBttn"}
+          nextLinkClassName={"nextBttn"}
+          disabledClassName={"paginationDisabled"}
+          activeClassName={"paginationActive"}
+        />
       </div>
-      {/* <Popup /> */}
-      {/* <div></div>
-      <div className="market-place"></div> */}
     </div>
   );
 };
---
+++ src/Pages/Market/data.ts
@@ -0,0 +1,167 @@
+import nft from "../../assets/nft.png";
+
+interface productsType {
+  Data: {
+    img: string;
+    name: string;
+    id: string;
+    price: number;
+  }[];
+}
+const productsData: productsType["Data"] = [
+  {
+    img: nft,
+    name: `LIZARD
+#15789`,
+    id: `#15789`,
+    price: 0.017,
+  },
+  {
+    img: nft,
+    name: `LIZARD
+#15789`,
+    id: `#15789`,
+    price: 0.017,
+  },
+  {
+    img: nft,
+    name: `LIZARD
+#15789`,
+    id: `#15789`,
+    price: 0.017,
+  },
+  {
+    img: nft,
+    name: `LIZARD
+#15789`,
+    id: `#15789`,
+    price: 0.017,
+  },
+  {
+    img: nft,
+    name: `LIZARD
+#15789`,
+    id: `#15789`,
+    price: 0.017,
+  },
+  {
+    img: nft,
+    name: `LIZARD
+#15789`,
+    id: `#15789`,
+    price: 0.017,
+  },
+  {
+    img: nft,
+    name: `LIZARD
+#15789`,
+    id: `#15789`,
+    price: 0.017,
+  },
+  {
+    img: nft,
+    name: `LIZARD
+#15789`,
+    id: `#15789`,
+    price: 0.017,
+  },
+  {
+    img: nft,
+    name: `LIZARD
+#15789`,
+    id: `#15789`,
+    price: 0.017,
+  },
+  {
+    img: nft,
+    name: `LIZARD
+#15789`,
+    id: `#15789`,
+    price: 0.017,
+  },
+  {
+    img: nft,
+    name: `LIZARD
+#15789`,
+    id: `#15789`,
+    price: 0.017,
+  },
+  {
+    img: nft,
+    name: `LIZARD
+#15789`,
+    id: `#15789`,
+    price: 0.017,
+  },
+  {
+    img: nft,
+    name: `LIZARD
+#15789`,
+    id: `#15789`,
+    price: 0.017,
+  },
+  {
+    img: nft,
+    name: `LIZARD
+#15789`,
+    id: `#15789`,
+    price: 0.017,
+  },
+  {
+    img: nft,
+    name: `LIZARD
+#15789`,
+    id: `#15789`,
+    price: 0.017,
+  },
+  {
+    img: nft,
+    name: `LIZARD
+#15789`,
+    id: `#15789`,
+    price: 0.017,
+  },
+  {
+    img: nft,
+    name: `LIZARD
+#15789`,
+    id: `#15789`,
+    price: 0.017,
+  },
+  {
+    img: nft,
+    name: `LIZARD
+#15789`,
+    id: `#15789`,
+    price: 0.017,
+  },
+  {
+    img: nft,
+    name: `LIZARD
+#15789`,
+    id: `#15789`,
+    price: 0.017,
+  },
+  {
+    img: nft,
+    name: `LIZARD
+#15789`,
+    id: `#15789`,
+    price: 0.017,
+  },
+  {
+    img: nft,
+    name: `LIZARD
+#15789`,
+    id: `#15789`,
+    price: 0.017,
+  },
+  {
+    img: nft,
+    name: `LIZARD
+#15789`,
+    id: `#15789`,
+    price: 0.017,
+  },
+];
+export default productsData;
